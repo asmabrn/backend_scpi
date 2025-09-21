6c42ecc8c4412e5bbc7ee97a33c2d1ee2ea92507
asmabrn <134811111+asmabrn@users.noreply.github.com>
2025-09-21 12:54:00 +0200

    Update scpis.js

--- routes/scpis.js
+++ routes/scpis.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Scpi = require('../models/scpi');
+const Scpi = require('../models/Scpi');
 const auth = require('../middleware/auth');
 
 
